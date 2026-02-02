769054e46dd1a7ccbb6de0661d17908fb614e192
AnkiteDi <ankitedigital@gmail.com>
2026-02-03 00:59:21 +0530

    Literals added

---
+++ 02 Data Types - Variables and Literals/03 Literals/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Literals");
+        //constant values used in a program are called literals
+        int value = 25;
+        //25 is a  literal and is integer literal
+
+        char c = 'A';   //A is a literal
+        String name = "JAVA";  //JAVA is a literal
+
+        float f = 67.7f;
+        double d = 3.13456;
+
+
+        long l = 7889988866L;
+        //to put as comma
+        long M = 23_000L;
+        System.out.println(M);
+    }
+}
