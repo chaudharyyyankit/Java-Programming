7e405f34d1a7b6c12ca6d9f37c7b5e507ff095f0
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-02-18 23:32:35 +0530

    Increment/Decrement Operator

---
+++ 04 Operators and Expressions/02 Increment_Decrement Operators/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Increment and Decrement Operator");
+
+        int x = 5;
+
+        x++; //6
+        ++x; //first x is 6 now it will become 7 and then assigned to x
+        System.out.println(x);
+    }
+}
