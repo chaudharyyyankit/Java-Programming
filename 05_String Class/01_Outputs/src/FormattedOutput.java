3748a5a1f6d6cc6e324f7c3bf32a8ac1527cecd0
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-03-01 22:08:57 +0530

    output

--- 05_String Class/01_Outputs/src/FormattedOutput.java
+++ 05_String Class/01_Outputs/src/FormattedOutput.java
@@ -1,2 +1,17 @@
 public class FormattedOutput {
+    static void main(String[] args) {
+        int x = 10;
+        float y = 12.25f;
+        char c = 'a';
+        /*
+        %d for integer
+        %f for float
+        %c for char
+        %s for string
+        %o octal 12.25->12
+        %x hexadecimal
+        %e for exponential form or in 10 power
+         */
+        System.out.printf("Hello %d %f %c World", x,y,c);
+    }
 }
